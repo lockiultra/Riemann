7b9f277c615ff4f2719add0ded53e7d834132263
lockiultra <36508851+lockiultra@users.noreply.github.com>
2023-02-23 21:17:31 +0300

    Update settings.py

--- riemann/riemann/settings.py
+++ riemann/riemann/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52ckro$wr*o)b_e_9dy_^%%idn-5-8rovcy^*yf-5#8z7js%6p'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
